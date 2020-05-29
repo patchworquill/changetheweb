0c6f700a1702c90232cb865d6accbc900adfe537
Dragos Rotaru <dragos@rotaru.co>
2020-05-29 18:02:07 -0400

    lets launch this badboy :)

---
+++ source/server.js
@@ -0,0 +1,161 @@
+const fs = require("fs");
+const WebSocket = require("ws");
+const express = require("express");
+const https = require("https");
+
+// TLS Encryption
+const credentials = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/changetheweb.xyz/privkey.pem",
+    "utf8"
+  ),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/changetheweb.xyz/fullchain.pem",
+    "utf8"
+  ),
+};
+
+const secret = process.env.SECRET;
+
+// If the secret key is not here, then the program should stop running immediately
+if (!secret) throw new Error("API key missing");
+
+// Names of folder and file locations on my personal computer
+const sourcePath = "/Home/dragos/changetheweb/source/";
+const metaPath = "/Home/dragos/changetheweb/source/meta.json";
+const informationPath = "/Home/dragos/changetheweb/information/";
+const logPath = "/Home/dragos/changetheweb/logs/server.log";
+
+/**
+ *
+ * @param {string} fileName
+ * @returns last fileExtension (string)
+ */
+const fileExtension = (fileName) => fileName.split(".").pop();
+
+/**
+ *
+ * @param {*} data any data
+ * @returns Digest in the form of a hexidecimal string
+ * sha256Digest takes any binary data (1s and 0s) as input, runs the SHA256 hashing algorithm on it
+ * and returns the result of that algorithm. The result is called a digest. The digest is also raw binary data,
+ * but we want to store the data in JSON which is UTF encoded and doesn't support binary,
+ * so next we encode the digest in hexidecimal and return it.
+ * (binary is base 2, decimal is base 10, hexidecimal is base 16).
+ */
+const sha256Digest = (data) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(data);
+  return hash.digest().toString("hex");
+};
+
+const app = express();
+app.use(express.static(sourcePath));
+app.use(express.static(informationPath));
+
+const server = https.createServer(credentials, app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log(`clients: ${wss.clients.size} `);
+  let alive = true;
+  const timeout = setTimeout(() => {
+    if (alive) {
+      ws.ping();
+      alive = false;
+    } else {
+      ws.terminate();
+    }
+  }, 5000 + 1000);
+
+  // Read the meta from disk as a UTF-8 String
+  const metaSerialized = fs.readFileSync(metaPath, {
+    encoding: "utf8",
+  });
+  const meta = JSON.parse(metaSerialized);
+  // Send Meta to sync with server.js
+  ws.send(
+    JSON.stringify({
+      secret,
+      meta,
+    })
+  );
+
+  ws.on("error", () => {
+    console.log("ws error");
+    timeout.refresh();
+    alive = true;
+  });
+  ws.on("message", () => {
+    console.log("ws message");
+    timeout.refresh();
+    alive = true;
+    const data = JSON.parse(message);
+    console.log(data);
+    // If message is from the watcher
+    if ((data.secret === secret && data.type = "update")) {
+      // Meta Not really needed because we can do a sync on connection and send only name + serialized + digest
+      // Update Source Meta
+      fs.writeFileSync(sourcePath + "meta.json", JSON.stringify(data.meta));
+
+      // Add Information Meta
+      const newMetaSerialized = JSON.stringify(meta, null, 2);
+      fs.writeFileSync(
+        informationPath + sha256Digest(newMetaSerialized),
+        newMetaSerialized
+      );
+      // Update Source Name
+      fs.writeFileSync(sourcePath + data.name, data.serialized);
+
+      // Add Information Name
+      fs.writeFileSync(
+        informationPath + sha256Digest(data.serialized),
+        data.serialized
+      );
+      // TODO deal with
+      // Broadcast to everyone else
+      const broadcastableData = {
+        ...data,
+        secret: undefined, // DO NOT LEAK API KEY
+      };
+      webSocketServer.clients.forEach((client) => {
+        if (client !== webSocket && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(broadcastableData));
+        }
+      });
+      if (data.name === "server.js") {
+        // TODO Mutate Server
+      }
+    }
+  });
+  ws.on("ping", () => {
+    console.log("ws ping");
+    timeout.refresh();
+    alive = true;
+  });
+  ws.on("pong", () => {
+    console.log("ws pong");
+    timeout.refresh();
+    alive = true;
+  });
+  ws.on("unexpected-response", () => {
+    console.log("ws unexpected-response");
+    timeout.refresh();
+    alive = true;
+  });
+  ws.on("upgrade", () => {
+    console.log("ws upgrade");
+    timeout.refresh();
+    alive = true;
+  });
+  ws.on("close", () => {
+    console.log(`clients: ${wss.clients.size} `);
+    console.log("ws close");
+    clearTimeout(timeout);
+  });
+});
+
+//start our server
+server.listen(443, () => {
+  console.log(`server started on port ${server.address().port}`);
+});
